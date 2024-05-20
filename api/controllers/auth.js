107bd5988cf0c7017e2d3abcd228b49839d0230f
kinchiee <vargasvincent28@gmail.com>
2024-05-20 17:49:51 +0800

    additional changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,8 +4,8 @@ import bcrypt from 'bcryptjs';
 export const register = (req, res) => {
 
     //Check if user exists
-    const q = "SELECT * FROM users WHERE username = ?"
-    db.query(q, [req.body.username], (err, data) => {
+    const query = "SELECT * FROM users WHERE username = ?"
+    db.query(query, [req.body.username], (err, data) => {
         if(err) return res.status(500).json(err)
         if(data.length) return res.status(409).json("User already exists!")
         //Create a new user
@@ -13,10 +13,10 @@ export const register = (req, res) => {
         const salt = bcrypt.genSaltSync(10);
         const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-        const q = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE(?)";
+        const query = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE(?)";
         const values = [req.body.username, req.body.email, hashedPassword, req.body.name];
 
-        db.query(q, [values], (err, data) => {
+        db.query(query, [values], (err, data) => {
             if (err) return res.status(500).json(err);
             return res.status(200).json("User has been created.")
         });
@@ -24,12 +24,12 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
-    const q = "SELECT * FROM users WHERE username = ?"
+    const query = "SELECT * FROM users WHERE username = ?"
 
-    db.query(q, [req.body.username], (err,data) => {
+    db.query(query, [req.body.username], (err,data) => {
         if (err) return res.status(500).json(err);
         if (data.length === 0) return res.status(400).json('User not found!');
-        
+
     })
 
 }

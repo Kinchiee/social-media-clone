94f87831364fe655a48b8751fbc55182f021fe5b
kinchiee <vargasvincent28@gmail.com>
2024-05-20 19:45:56 +0800

    function up to register and login

--- api/index.js
+++ api/index.js
@@ -9,8 +9,14 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 //Middlewares
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+})
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+}));
 app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes);
@@ -19,6 +25,6 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/comments", commentRoutes);
 app.use("/api/v1/likes", likeRoutes);
 
-app.listen(8800, () => {
+app.listen(8000, () => {
     console.log("API WORKING")
 });

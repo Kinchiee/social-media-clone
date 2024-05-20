8a283d09527c3b44e802b2b365b2ce55c38b6ac5
kinchiee <vargasvincent28@gmail.com>
2024-05-20 17:47:42 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import commentRoutes from './routes/comments.js';
+import likeRoutes from './routes/likes.js';
+
+//Middlewares
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/likes", likeRoutes);
+
+app.listen(8800, () => {
+    console.log("API WORKING")
+});
